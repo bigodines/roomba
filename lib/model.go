5de1aa18c4bbdc0314ec5ce747ed19865af4ffc8
matheus mendes <bigodines@gmail.com>
2019-10-29 22:20:07 -0700

    Stats callback (#5)
    * skeleton for stats
    
    * add payload struct. tests failing
    
    * tests passing
    
    * actually callback
    
    * tests
    
    * move and rename Entry to PullRequest
    
    * it works with api@master

--- lib/model.go
+++ lib/model.go
@@ -11,25 +11,25 @@ type (
 	// ReportPayload is the payload Roomba will post to the report callback URL (see config.go for details)
 	// It contains a mix of Config{}, Record{} and runtime information
 	ReportPayload struct {
-		ChannelID string        `json:"channel_id"`
-		Datetime  time.Time     `json:"datetime"`
-		PRs       []PullRequest `json:"prs"`
-		Reminders []Reminder    `json:"reminders"`
+		ChannelID string        `json:"channel_id,omitempty"`
+		Datetime  time.Time     `json:"datetime,omitempty"`
+		PRs       []PullRequest `json:"prs,omitempty"`
+		Reminders []Reminder    `json:"reminders,omitempty"`
 	}
 
 	Reminder struct {
-		Date time.Time `json:"date"`
-		Text string    `json:"text"`
+		Date time.Time `json:"date,omitempty"`
+		Text string    `json:"text,omitempty"`
 	}
 
 	// PullRequest represents the important fields for Roomba in a Pull Request
 	PullRequest struct {
-		Repository string    `json:"repository"`
-		Author     string    `json:"author"`
-		UpdatedAt  time.Time `json:"updated_at"`
-		Labels     string    `json:"labels"`
-		Title      string    `json:"title"`
-		Permalink  string    `json:"permalink"`
+		Repository string    `json:"repository,omitempty"`
+		Author     string    `json:"author,omitempty"`
+		UpdatedAt  time.Time `json:"updated_at,omitempty"`
+		Labels     string    `json:"labels,omitempty"`
+		Title      string    `json:"title,omitempty"`
+		Permalink  string    `json:"permalink,omitempty"`
 	}
 )
 
